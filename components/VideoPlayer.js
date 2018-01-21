2a5744296da97654c3ffa2effbfdc8f6b61f73d5
Sandip Nirmal <sandipnirmal@icloud.com>
2018-01-21 20:14:21 +0530

    setting env configs

--- components/VideoPlayer.js
+++ components/VideoPlayer.js
@@ -4,7 +4,6 @@ import {WebView} from 'react-native';
 import style from './../styles/styles';
 
 const VideoPlayer = (props) => {
-    console.log(props.url);
     return (
         <WebView
             style={[style.videoPlayerContainer, style.backdropSize]}
--- utilities/constants.js
+++ utilities/constants.js
@@ -1,4 +1,5 @@
 import {Dimensions} from 'react-native';
+import {API_KEY} from 'react-native-dotenv';
 
 // return device width and height
 // const {height:winHeight, width:winWidth} = Dimensions.get('window');
@@ -7,7 +8,7 @@ const tabBarHeight = 54;
 
 const APP_CONSTANT = {
     api_base_url: 'https://api.themoviedb.org/3',
-    api_key: 'api_key=b8a04ea374eece868a6690782c9e7536',
+    api_key: API_KEY,
     lan_region: '&language=IN-hi&region=IN&page=1',
     goldenRatio: 1.618,
     width,
