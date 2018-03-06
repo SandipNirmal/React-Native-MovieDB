7f11d925a6224fc7c7f10783342243fb2fe1123c
Umesh Kadam <umeshkflance@gmail.com>
2018-03-06 17:19:00 +0530

    Add back button support for android

--- src/components/AppNavigation.js
+++ src/components/AppNavigation.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
+import { BackHandler } from 'react-native'
 import { connect } from 'react-redux'
 import { addNavigationHelpers } from 'react-navigation'
+import { NavigationActions } from 'react-navigation'
 import MainScreen from '../router'
+
+// Refer https://reactnavigation.org/docs/redux-integration.html
 
 // following is to resolve issue https://github.com/infinitered/ignite/issues/1225
 // for react-navigation 1.0.0-beta.30
@@ -18,6 +22,31 @@ const addListener = createReduxBoundAddListener('root')
 // end for react-navigation 1.0.0-beta.30
 
 class AppNavigation extends Component {
+  componentDidMount() {
+    BackHandler.addEventListener('hardwareBackPress', this.onBackPress)
+  }
+
+  componentWillUnmount() {
+    BackHandler.removeEventListener('hardwareBackPress', this.onBackPress)
+  }
+
+  /**
+   * Close the app if we are on the main screen of the current tab and a back
+   * button is hit
+   */
+  closeApp = () => {
+    const { navigationState } = this.props
+    const index = navigationState.index
+    // Are we on the main screen of the tab ? i.e. not on any details page
+    return navigationState.routes[index].index == 0 ? true : false
+  }
+
+  onBackPress = () => {
+    if (this.closeApp()) return false
+    this.props.dispatch(NavigationActions.back())
+    return true
+  };
+
   render () {
     const { navigationState, dispatch } = this.props
     return (
