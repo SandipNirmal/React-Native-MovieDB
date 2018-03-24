4aebf9c29c500c0b6cfc67c6083f8f21a2691a73
Sandip Nirmal <sandipnirmal@icloud.com>
2018-03-24 19:14:56 +0530

    Added eslint support

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,15 +1,15 @@
-import React, {Component} from 'react'
-import {View, Text, StyleSheet, ActivityIndicator} from 'react-native'
-import {SearchBar, ButtonGroup} from 'react-native-elements'
-import * as _ from 'lodash'
-import axios from 'axios'
-import {NavigationActions} from 'react-navigation'
-import {connect} from 'react-redux'
-import {styles} from 'react-native-theme'
+import React, { Component } from "react";
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
+import { SearchBar, ButtonGroup } from "react-native-elements";
+import * as _ from "lodash";
+import axios from "axios";
+import { NavigationActions } from "react-navigation";
+import { connect } from "react-redux";
+import { styles } from "react-native-theme";
 
-import {primaryColor} from './../../styles/styles';
-import Constant from '../../utilities/constants';
-import SearchResult from './SearchResult';
+import { primaryColor } from "./../../styles/styles";
+import Constant from "../../utilities/constants";
+import SearchResult from "./SearchResult";
 import {
   doneSearchingMoviesEtc,
   searchFilterChanged,
@@ -17,55 +17,53 @@ import {
   searchResultSelected,
   selectedMovie,
   selectedTvShow
-} from '../../Actions';
+} from "../../Actions";
 
-import { searchItem } from '../../services/search'
-  
-const buttons = ['Movie', 'Tv', 'Person']
+import { searchItem } from "../../services/search";
+
+const buttons = ["Movie", "Tv", "Person"];
 
 class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: ''
-    }
+      value: ""
+    };
   }
 
-  onTextChange = (e) => {
+  onTextChange = e => {
     // Set value
-    this.setState({value: e});
-    _.debounce(this.onSearch, 500)()
-  }
+    this.setState({ value: e });
+    _.debounce(this.onSearch, 500)();
+  };
 
   onSearch = () => {
     // Search only if there is any value
-    const {value} = this.state
+    const { value } = this.state;
     if (value) {
       this.props.onSearchingForMoviesEtc();
-      const { settings: { language, region } } = this.props
+      const { settings: { language, region } } = this.props;
 
       searchItem(value, language, region)
-      .then(({data}) => {
-        this.props.onDoneSearchingMoviesEtc(data.results)
-      })
-      .catch(error => console.log(error.response))
+        .then(({ data }) => {
+          this.props.onDoneSearchingMoviesEtc(data.results);
+        })
+        .catch(error => console.log(error.response));
     }
-  }
+  };
 
   onClearText = () => {
-    this.setState({value: ''})
-  }
+    this.setState({ value: "" });
+  };
 
   /**
    * Filters search results based on media_type
    */
   filterSearchResults = (results, index) => {
-    return results.filter((result) => {
-      return result
-        .media_type
-        .toLowerCase() === buttons[index].toLowerCase()
-    })
-  }
+    return results.filter(result => {
+      return result.media_type.toLowerCase() === buttons[index].toLowerCase();
+    });
+  };
 
   render() {
     const {
@@ -81,10 +79,16 @@ class Search extends Component {
 
     return (
       <View style={[{ flex: 1 }, styles.screenBackgroundColor]}>
-        <SearchBar style={{
-          marginTop: 20
-        }} // lightTheme
-          round onChangeText={this.onTextChange} onClearText={this.onClearText} placeholder='Search' value={this.state.value}/>
+        <SearchBar
+          style={{
+            marginTop: 20
+          }} // lightTheme
+          round
+          onChangeText={this.onTextChange}
+          onClearText={this.onClearText}
+          placeholder="Search"
+          value={this.state.value}
+        />
 
         <ButtonGroup
           lightTheme={false}
@@ -92,20 +96,24 @@ class Search extends Component {
           selectedIndex={selectedIndex}
           buttons={buttons}
           containerStyle={{
-          height: 30,
-          backgroundColor: '#e1e1e1',
-          marginTop: 10
-        }}/>
+            height: 30,
+            backgroundColor: "#e1e1e1",
+            marginTop: 10
+          }}
+        />
 
         <View>
-          {isSearching
-            ? <ActivityIndicator size="large" color={primaryColor}/>
-            : <SearchResult
+          {isSearching ? (
+            <ActivityIndicator size="large" color={primaryColor} />
+          ) : (
+            <SearchResult
               config={config}
               items={filteredResults}
               popular={popular}
               onSelectPopular={this.onTextChange}
-              onSelect={onSearchResultSelected}/>}
+              onSelect={onSearchResultSelected}
+            />
+          )}
         </View>
       </View>
     );
@@ -116,38 +124,44 @@ const mapStateToProps = state => ({
   config: state.configuration,
   popular: state.movies.categories.popular,
   settings: state.settings,
-  ...state.search,
+  ...state.search
 });
 
 const mapDispatchToProps = dispatch => ({
-  onFilterChanged: (index) => {
+  onFilterChanged: index => {
     dispatch(searchFilterChanged(index));
   },
   onSearchingForMoviesEtc: () => {
     dispatch(searchingForMoviesEtc());
   },
-  onDoneSearchingMoviesEtc: (results) => {
+  onDoneSearchingMoviesEtc: results => {
     dispatch(doneSearchingMoviesEtc(results));
   },
-  onSearchResultSelected: (result) => {
+  onSearchResultSelected: result => {
     const params = {
       name: result.name || result.title,
       id: result.id
-    }
+    };
 
     dispatch(searchResultSelected(result, result.media_type));
     switch (result.media_type) {
-      case 'movie':
-        dispatch(NavigationActions.navigate({routeName: 'MovieDetails', params}));
+      case "movie":
+        dispatch(
+          NavigationActions.navigate({ routeName: "MovieDetails", params })
+        );
         break;
-      case 'tv':
-        dispatch(NavigationActions.navigate({routeName: 'TvShowDetails', params}));
+      case "tv":
+        dispatch(
+          NavigationActions.navigate({ routeName: "TvShowDetails", params })
+        );
         break;
-      case 'person':
-        dispatch(NavigationActions.navigate({routeName: 'CastDetails', params}));
+      case "person":
+        dispatch(
+          NavigationActions.navigate({ routeName: "CastDetails", params })
+        );
         break;
       default:
-        console.log('Unrecognised media type');
+        console.log("Unrecognised media type");
         break;
     }
   }
